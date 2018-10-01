e2c1536a657dc5ae155085afe0adfd19af2711a9
Fedor Paretsky <me@f3d0r.com>
2018-10-01 13:43:29 -0700

    Re-init repo

---
+++ Podfile
@@ -0,0 +1,18 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'aspace' do
+
+    pod 'Mapbox-iOS-SDK', '~> 4.4'
+    pod "SHSearchBar"
+
+    target 'aspaceTests' do
+        inherit! :search_paths
+        # Pods for testing
+    end
+
+    target 'aspaceUITests' do
+        inherit! :search_paths
+        # Pods for testing
+    end
+end
---
+++ aspace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aspace
+//
+//  Created by Fedor Paretsky on 10/1/18.
+//  Copyright © 2018 aspace, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
